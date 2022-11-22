31065c910de0a7ff8389cd6c77d7badf55edce24
l226648 Uzair Ahmad <l226648@lhr.nu.edu.pk>
2022-11-23 01:19:20 +0500

    Entered a cout message

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,16 @@
 #include <SFML/Graphics.hpp>
+#include<iostream>
+using namespace std;
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Test Window");
 	sf::Event event;
 	while (window.isOpen()) {
 		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
+			if (event.type == sf::Event::Closed){
 				window.close();
 			}
 		}
 	}
+	cout << "Hello World";
 }
