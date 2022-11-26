b89255e8bc714079484e0c4d48851d5baad9f9f3
bobsorvagana <daniyalakbar19@gmail.com>
2022-11-26 17:33:34 +0500

    learning something new

--- Main.cpp
+++ Main.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Test Window");
-	sf::Event event;
+	sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Window", sf::Style::Default);
+
 	while (window.isOpen()) {
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed){
+		sf::Event evnt;
+		while (window.pollEvent(evnt)) {
+			if (evnt.type == evnt.Closed){
 				window.close();
 			}
 		}
 	}
-	cout << "Hello World";
 }
